30620a09d971b763df26db20aadaecef80a5f23d
shivasj <shivasj@gmail.com>
2024-09-15 10:37:11 -0400

    Updated shell file

--- dataset-creator/docker-shell.sh
+++ dataset-creator/docker-shell.sh
@@ -3,10 +3,11 @@
 # exit immediately if a command exits with a non-zero status
 set -e
 
+
 # Read the settings file
 source ../env.dev
 
-export IMAGE_NAME="dataset-creator"
+export IMAGE_NAME="llm-dataset-creator"
 
 # Build the image based on the Dockerfile
 docker build -t $IMAGE_NAME -f Dockerfile .
@@ -17,5 +18,4 @@ docker run --rm --name $IMAGE_NAME -ti \
 -v "$SECRETS_DIR":/secrets \
 -e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
 -e GCP_PROJECT=$GCP_PROJECT \
--e GCS_BUCKET_NAME=$GCS_BUCKET_NAME \
 $IMAGE_NAME
